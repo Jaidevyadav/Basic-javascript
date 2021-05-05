ac66cab730e55caa0475d5a805090a226fa02919
Jaidevyadav <Jaidevyadav69@gmail.com>
2021-05-05 12:36:16 +0530

    jaidevyadav/javascript-project

--- My Java App/index.js
+++ My Java App/index.js
@@ -1,10 +1,15 @@
-var cars = ["valvo", "hyundai", "Honda"];
-// var person = {name : "jai" ,age = 31};
-// person.name = 'Rita';
+//var cars = ["valvo", "hyundai", "Honda"];
+
+// Here im checking different methods to call value from arrays
+// var person = {name : "jai" ,age = 31};       //creating a object and passing its properties.
+// person.name = 'Rita';            //dot method
 // console.log(person.name);
 
-// let firstGame = "jaidev";
-// let word = 21;
-// let isComing = true;
-// let firstMap = "";
-// let cat = null;
+// here i was checking different type of value which are passed to variables.
+let firstGame = "jaidev";
+let word = 21;
+let isComing = true;
+let firstMap = "";
+let cat = null;
+
+console.log(firstGame);
