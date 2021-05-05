b3d421cbde839c22aef3de50efbe2f1644a3ac00
Jaidevyadav <Jaidevyadav69@gmail.com>
2021-05-05 12:35:49 +0530

    jaidevyadav/python=project

---
+++ My Java App/index.js
@@ -0,0 +1,10 @@
+var cars = ["valvo", "hyundai", "Honda"];
+// var person = {name : "jai" ,age = 31};
+// person.name = 'Rita';
+// console.log(person.name);
+
+// let firstGame = "jaidev";
+// let word = 21;
+// let isComing = true;
+// let firstMap = "";
+// let cat = null;
