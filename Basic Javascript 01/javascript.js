a54344ef59876a4bff3189da534592b8fd6b378a
Jaidevyadav <Jaidevyadav69@gmail.com>
2021-05-06 08:37:46 +0530

    Javascript

---
+++ Basic Javascript 01/javascript.js
@@ -0,0 +1,11 @@
+function greet(name, Lastname) {
+  //name is parameter here
+  console.log("Hello " + name + " " + Lastname);
+}
+
+function square(number) {
+  return number * number;
+}
+
+greet("Jhon", "Silvasatrone"); //here we pass argument to parametere name
+console.log(square(2));
